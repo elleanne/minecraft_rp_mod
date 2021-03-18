214f6887cc7228aabdb13c16f99bfe82003f3e2a
Ali-Doggaz <charfedine611@gmail.com>
2021-03-18 19:56:12 +0100

    Implement Bank Robbery Feature (#28)

--- src/main/java/mlh/goofygoofies/minecraft_rp/MinecraftRP.java
+++ src/main/java/mlh/goofygoofies/minecraft_rp/MinecraftRP.java
@@ -3,10 +3,12 @@ package mlh.goofygoofies.minecraft_rp;
 import net.skinsrestorer.api.SkinsRestorerAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,26 +17,38 @@ import org.bukkit.entity.Player;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.bukkit.Bukkit.getServer;
+
 public class MinecraftRP extends JavaPlugin implements Listener {
     private SkinsRestorerAPI skinsRestorerAPI;
     Map<Integer, String> playersJobsList = new HashMap<>();
     final static LandClaims lc = new LandClaims();
+    Location BANK_LOCATION;
 
     @Override
     public void onEnable() {
+        BANK_LOCATION = new Location(getServer().getWorlds().get(0), 7000, 86, -5110);
         // SkinsRestorer
         getLogger().info("Loading SkinsRestorer API...");
         skinsRestorerAPI = SkinsRestorerAPI.getApi();
         getLogger().info(skinsRestorerAPI.toString());
 
+        //gives AllPLayers class the plugin's object (used for multi-threading)
+        AllPlayersCommands.plugin = this;
+
         // Commands
         getCommand("job").setExecutor(new JobsCommand(playersJobsList));
         getCommand("heal").setExecutor(new DoctorCommands(playersJobsList));
+
+  
         AllPlayersCommands ac = new AllPlayersCommands();
         getCommand("me").setExecutor(ac);
         getCommand("id").setExecutor(ac);
         getCommand("it").setExecutor(ac);
         getCommand("roll").setExecutor(ac);
+        getCommand("rob").setExecutor(ac);
+
+
         GuardCommands gc = new GuardCommands(playersJobsList);
         getCommand("inspect").setExecutor(gc);
         getCommand("jail").setExecutor(gc);
@@ -45,7 +59,6 @@ public class MinecraftRP extends JavaPlugin implements Listener {
         getCommand("selfheal").setExecutor(lc);
         getCommand("unclaim").setExecutor(lc);
         
-        // Events
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(new EnderChestManager(), this);
         pm.registerEvents(this, this);
@@ -78,6 +91,28 @@ public class MinecraftRP extends JavaPlugin implements Listener {
                     player.setHealth(player.getHealth() + 0.5);
             }
         }, 140L);
+        return;
+    }
+
+    @EventHandler
+    /*
+     * If the bank is getting robbed, checks if the player who is robbing it
+     * leaves the bank's zone. If he did, aborts the robbery.
+     */
+    public void robberExitsArea(PlayerMoveEvent event) {
+        if (!(event.getPlayer() instanceof Player))
+            return;
+        if (AllPlayersCommands.robber == null) return;
+        if (event.getPlayer().getEntityId() != AllPlayersCommands.robber.getEntityId() ) return;
+        //checks if player is leaving the bank area
+        if(event.getPlayer().getLocation().distance(BANK_LOCATION) > 50 ){
+            event.getPlayer().sendMessage("You are too far from the bank! The robbery failed...");
+            AllPlayersCommands.robber = null;
+            int old_robbing_task_id = AllPlayersCommands.robbing_task_id;
+            AllPlayersCommands.robbing_task_id = 0;
+            Bukkit.getServer().getScheduler().cancelTask(old_robbing_task_id);
+        }
+
         return;
     }
 
